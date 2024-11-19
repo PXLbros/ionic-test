2c41de26e30bd757e63624bd490073fae86bb576
mkettel <mattkettelkamp@gmail.com>
2024-11-19 15:58:53 -0800

    updated and showing games in games page

--- src/views/fair/GamesPage.vue
+++ src/views/fair/GamesPage.vue
@@ -10,8 +10,11 @@
                 </ion-toolbar>
               </ion-header>
 
-            <div class="home">
-                HI from Games
+            <div class="main">
+                <h1 class="main__header">Games</h1>
+                <div v-if="games" v-for="game in games" :key="game.id">
+                    <h2>{{ game.name }}</h2>
+                </div>
             </div>
 
         </ion-content>
@@ -21,9 +24,28 @@
 
 <script setup lang="ts">
     import { IonContent, IonPage, IonHeader, IonToolbar, IonTitle, IonButtons, IonBackButton } from '@ionic/vue';
+    import { useDataStore } from '@/stores/data';
+
+    const dataStore = useDataStore();
+
+    // const VNode = dataStore?.data?.mobileApp ? StrapiBlocks({ content: dataStore.data.mobileApp.homePage.introText }) : null;
+
+    console.log('data from the store', dataStore.data);
+
+    const games = dataStore?.data?.nysfairWebsite.games;
+    console.log('games', games);
 
 </script>
 
 <style lang="scss" scoped>
+
+.main {
+    padding: 40px;
+
+    &__header {
+        font-size: 28px;
+        font-weight: bold;
+    }
+}
 
 </style>
