f6702c183b4d15781f8b35094e0206e668af6411
mkettel <mattkettelkamp@gmail.com>
2024-12-03 07:35:35 -0800

    updated some more things

--- src/views/fair/MusicDetailPage.vue
+++ src/views/fair/MusicDetailPage.vue
@@ -12,7 +12,8 @@
 
             <div v-if="event" class="main">
                 <div class="main__image">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="62" height="62" viewBox="0 0 62 62" fill="none">
+                    <img v-if="event.featured_image" :src="event.featured_image" alt="">
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" width="62" height="62" viewBox="0 0 62 62" fill="none">
                         <path d="M62 55.1111V6.88889C62 3.1 58.9 0 55.1111 0H6.88889C3.1 0 0 3.1 0 6.88889V55.1111C0 58.9 3.1 62 6.88889 62H55.1111C58.9 62 62 58.9 62 55.1111ZM18.9444 36.1667L27.5556 46.5344L39.6111 31L55.1111 51.6667H6.88889L18.9444 36.1667Z" fill="#1E5EAE"/>
                     </svg>
                 </div>
@@ -123,6 +124,14 @@ ion-back-button {
         align-items: center;
         justify-content: center;
         margin-bottom: 24px;
+        overflow: hidden;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 24px;
+        }
     }
 
     &__content {
