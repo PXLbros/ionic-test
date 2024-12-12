d446b3562a1674eca3bb1c56eaa3099597b46218
majesticblvd <dennis@pxlagency.com>
2024-12-12 14:04:33 -0800

    Add Sentry

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import router from './router';
 import { IonicVue } from '@ionic/vue';
 import { createPinia } from 'pinia';
 
+import * as Sentry from '@sentry/capacitor';
+import * as SentryVue from '@sentry/vue';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
@@ -41,6 +44,30 @@ const app = createApp(App)
   .use(IonicVue)
   .use(router)
   .use(pinia);
+
+Sentry.init(
+  {
+    app,
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+
+    environment: import.meta.env.VITE_NODE_ENV,
+
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      // Sentry.replayIntegration(),
+    ],
+
+    tracesSampleRate: 1.0, // 1.0 = Capture 100% of the transactions
+
+    // // Capture Replay for 10% of all sessions,
+    // // plus for 100% of sessions with an error
+    // // Learn more at
+    // // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+    // replaysSessionSampleRate: 0.1,
+    // replaysOnErrorSampleRate: 1.0,
+  },
+  SentryVue.init
+);
 
 router.isReady().then(() => {
   app.mount('#app');
