cb687900685de936eee84139005ad751a08da2f9
majesticblvd <dennis@pxlagency.com>
2025-01-02 18:50:45 -0800

    Fix so the event heart icon updates after clicking add/remove from favorites

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,6 @@ module.exports = {
     'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
+    'prefer-const': 'warn',
   }
 }
