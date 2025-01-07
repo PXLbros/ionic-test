47143450378c19abdc4cfd97102050ff22574e36
majesticblvd <dennis@pxlagency.com>
2025-01-06 18:14:44 -0800

    Formatting

--- src/views/fair/DailySchedulePage.vue
+++ src/views/fair/DailySchedulePage.vue
@@ -1,94 +1,94 @@
 <template>
-    <ion-page>
-        <ion-header :translucent="true">
-            <ion-toolbar>
-                <ion-buttons slot="start">
-                    <ion-back-button default-href="/fair"></ion-back-button>
-                </ion-buttons>
-                <ion-title>Schedule</ion-title>
-            </ion-toolbar>
-        </ion-header>
-        <ion-content :fullscreen="true">
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+            <ion-back-button default-href="/fair"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Schedule</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
 
-            <div class="main">
-                <div class="main__header">
-                    <div class="main__header-img">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="62" height="62" viewBox="0 0 62 62" fill="none">
-                            <path d="M62 55.1111V6.88889C62 3.1 58.9 0 55.1111 0H6.88889C3.1 0 0 3.1 0 6.88889V55.1111C0 58.9 3.1 62 6.88889 62H55.1111C58.9 62 62 58.9 62 55.1111ZM18.9444 36.1667L27.5556 46.5344L39.6111 31L55.1111 51.6667H6.88889L18.9444 36.1667Z" fill="#1E5EAE"/>
-                        </svg>
-                    </div>
-                    <div class="main__header-content">
-                        <h1 class="title">Daily Schedule</h1>
-                        <p class="subtitle">Class ridiculus rhoncus ad suspendisse ridiculus malesuada; litora morbi</p>
-                    </div>
-                </div>
+    <div class="main">
+      <div class="main__header">
+        <div class="main__header-img">
+          <svg xmlns="http://www.w3.org/2000/svg" width="62" height="62" viewBox="0 0 62 62" fill="none">
+              <path d="M62 55.1111V6.88889C62 3.1 58.9 0 55.1111 0H6.88889C3.1 0 0 3.1 0 6.88889V55.1111C0 58.9 3.1 62 6.88889 62H55.1111C58.9 62 62 58.9 62 55.1111ZM18.9444 36.1667L27.5556 46.5344L39.6111 31L55.1111 51.6667H6.88889L18.9444 36.1667Z" fill="#1E5EAE"/>
+          </svg>
+        </div>
+        <div class="main__header-content">
+          <h1 class="title">Daily Schedule</h1>
+          <p class="subtitle">Class ridiculus rhoncus ad suspendisse ridiculus malesuada; litora morbi</p>
+        </div>
+      </div>
 
+      <div v-if="dates" class="date-selector">
+        <div class="date-selector__container">
+          <button
+            v-for="(date, index) in dates"
+            :key="index"
+            class="date-card"
+            :class="{ 'date-card--active': selectedDateIndex === index }"
+            @click="selectDate(index)"
+          >
+            <div class="date-card__day">{{ date.dayName || "Date Name" }}</div>
+            <div class="date-card__date">Day {{ date.day || "Date Day" }}</div>
+          </button>
+        </div>
+      </div>
 
+      <div class="filter-section">
+        <div class="category-filter">
+          <select class="category-select"  v-model="selectedCategory" @change="onCategoryChange">
+            <option value="all">All Categories</option>
+            <option v-for="category in categories" :key="category.id" :value="category.id">
+              {{ category.name }}
+            </option>
+          </select>
+        </div>
+      </div>
 
-                <div v-if="dates" class="date-selector">
-                    <div class="date-selector__container">
-                        <button
-                        v-for="(date, index) in dates"
-                        :key="index"
-                        class="date-card"
-                        :class="{ 'date-card--active': selectedDateIndex === index }"
-                        @click="selectDate(index)"
-                        >
-                        <div class="date-card__day">{{ date.dayName || "Date Name" }}</div>
-                        <div class="date-card__date">Day {{ date.day || "Date Day" }}</div>
-                    </button>
-                </div>
+      <div class="loader-container" v-if="isLoading || isDateChanging">
+        <div class="spinner"></div>
+        <p>Loading Schedule...</p>
+      </div>
+
+        <div v-else class="schedule-content">
+          <div v-if="filteredEvents.length === 0" class="no-events">
+            <p>No events scheduled for this day</p>
+          </div>
+
+          <div v-else class="events-list">
+            <div v-for="event in filteredEvents" :key="event.id" class="event-item">
+              <div class="content">
+                <h3>{{ event.title || "Event Title" }}</h3>
+                <p>{{ event.start_time || "Event Start Time" }} </p>
+                <p>{{ event.venue.name || "Event Venue N/A" }}</p>
+                <p v-if="event.categories.length > 0">
+                  Categories: {{ event.categories.map(catId => getCategoryName(catId)).join(', ') }}
+                </p>
+              </div>
+
+              <div class="favorite">
+                <button
+                  class="favorite-button"
+                  :class="{ 'is-favorite': event.dateDetails.isFavorite }"
+                  @click="toggleFavorite(event.id, event.dateDetails)"
+                  :disabled="event.dateDetails.isAddingToFavorites || event.dateDetails.isRemovingFromFavorites"
+                >
+                  <ion-icon
+                    :icon="event.dateDetails.isFavorite ? heart : heartOutline"
+                    :class="{ 'loading': event.dateDetails.isAddingToFavorites || event.dateDetails.isRemovingFromFavorites }"
+                  ></ion-icon>
+                </button>
+              </div>
             </div>
-            <div class="filter-section">
-                   <div class="category-filter">
-                        <select class="category-select"  v-model="selectedCategory" @change="onCategoryChange">
-                            <option value="all">All Categories</option>
-                           <option v-for="category in categories" :key="category.id" :value="category.id">
-                             {{ category.name }}
-                           </option>
-                        </select>
-                   </div>
-            </div>
-
-                <div class="loader-container" v-if="isLoading || isDateChanging">
-                      <div class="spinner"></div>
-                      <p>Loading Schedule...</p>
-                </div>
-
-                <div v-else class="schedule-content">
-                     <div v-if="filteredEvents.length === 0" class="no-events">
-                        <p>No events scheduled for this day</p>
-                     </div>
-                    <div v-else class="events-list">
-                        <div v-for="event in filteredEvents" :key="event.id" class="event-item">
-                            <div class="content">
-                                <h3>{{ event.title || "Event Title" }}</h3>
-                                <p>{{ event.start_time || "Event Start Time" }} </p>
-                                <p>{{ event.venue.name || "Event Venue N/A" }}</p>
-                                <p v-if="event.categories.length > 0">
-                                    Categories: {{ event.categories.map(catId => getCategoryName(catId)).join(', ') }}
-                                </p>
-                            </div>
-
-                            <div class="favorite">
-                              <button
-                                class="favorite-button"
-                                :class="{ 'is-favorite': event.dateDetails.isFavorite }"
-                                @click="toggleFavorite(event.id, event.dateDetails)"
-                                :disabled="event.dateDetails.isAddingToFavorites || event.dateDetails.isRemovingFromFavorites"
-                              >
-                                <ion-icon
-                                  :icon="event.dateDetails.isFavorite ? heart : heartOutline"
-                                  :class="{ 'loading': event.dateDetails.isAddingToFavorites || event.dateDetails.isRemovingFromFavorites }"
-                                ></ion-icon>
-                              </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </ion-content>
-    </ion-page>
+          </div>
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
