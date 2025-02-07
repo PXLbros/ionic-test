d2909d6effaeddb648b7bac566b54c1597158cae
Dennis Nygren <dennis@dreamhuman.com>
2025-02-07 12:42:21 -0800

    Show tickets menu item even when no ticket URL is present

--- src/views/fair/FairPage.vue
+++ src/views/fair/FairPage.vue
@@ -12,7 +12,7 @@
                 </div>
                 <ion-grid>
                     <ion-row>
-                        <ion-col v-if="ticketsUrl" @click="openTicketsWebsite">
+                        <ion-col @click="openTicketsWebsite">
                             <div class="main-button">
                                 <svg class="main-button__icon"  viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                                   <rect width="80" height="80" fill="url(#pattern0_1820_157)"/>
