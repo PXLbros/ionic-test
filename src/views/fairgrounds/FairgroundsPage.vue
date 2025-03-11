3098bd2ceb526e60485f47c6fd0832bfece3e026
mkettel <mattkettelkamp@gmail.com>
2025-03-11 08:16:22 -0700

    home page carousel working

--- src/views/fairgrounds/FairgroundsPage.vue
+++ src/views/fairgrounds/FairgroundsPage.vue
@@ -9,31 +9,7 @@
             <div class="main__header-title">
                 <h1 id="fg-title">Upcoming Events</h1>
             </div>
-              <div class="main__header">
-                  <div v-if="featuredEvents && featuredEvents.length"
-                          v-for="event in featuredEvents"
-                          :key="event.id"
-                          class="main__header-card"
-                      >
-                      <div class="image-container">
-                          <img
-                              v-if="event.eventImage && event.eventImage.length"
-                              :src="event.eventImage[0].url"
-                              :alt="event.eventImage[0].title"
-                              class="event-image"
-                          />
-                          <div v-else class="placeholder-image">
-                              <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                  <path d="M62 55.1111V6.88889C62 3.1 58.9 0 55.1111 0H6.88889C3.1 0 0 3.1 0 6.88889V55.1111C0 58.9 3.1 62 6.88889 62H55.1111C58.9 62 62 58.9 62 55.1111ZM18.9444 36.1667L27.5556 46.5344L39.6111 31L55.1111 51.6667H6.88889L18.9444 36.1667Z" fill="#1E5EAE"/>
-                              </svg>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="image-banner">
-                    <p class="date">Feb 27th, 2025</p>
-                    <h3 class="title">Spring NYS RV Show</h3>
-                  </div>
-              </div>
+                <EventCarousel :eventData="allEvents" />
 
               <!-- Updated grid section to match the design -->
               <div class="categories-section">
@@ -93,6 +69,30 @@
   console.log('dataStore', dataStore.data);
 
   const featuredEvents = dataStore.data.nysfairgroundsWebsite.featuredEvents;
+
+  // Combine featured events with other events to create a full list of events
+  const allEvents = computed(() => {
+    const featured = dataStore.data.nysfairgroundsWebsite.featuredEvents || [];
+    const regularEvents = dataStore.data.nysfairgroundsWebsite.events || [];
+
+    // If there are no featured events, just use the first 5 regular events
+    if (!featured.length) {
+      return regularEvents.slice(0, 5);
+    }
+
+    // Otherwise, use featured events and add regular events to get to 5 total
+    const combined = [...featured];
+    if (combined.length < 5) {
+      const remainingNeeded = 5 - combined.length;
+      // Add regular events that are not already in featured
+      const regularNotFeatured = regularEvents.filter(
+        (regEvent: { id: string }) => !featured.some((featEvent: any) => featEvent.id === regEvent.id)
+      );
+      combined.push(...regularNotFeatured.slice(0, remainingNeeded));
+    }
+
+    return combined;
+  });
 </script>
 
 <style lang="scss" scoped>
