f9a426f464db47abe5834015c7fc9d0876eec0ce
majesticblvd <dennis@dreamhuman.com>
2025-04-08 18:33:30 -0700

    Various

---
+++ src/composables/useLogger.ts
@@ -0,0 +1,11 @@
+import { ref } from 'vue';
+import { createLogger } from '../utils/logger';
+
+export const useLogger = ({
+  name,
+  debugEnabled,
+}: { name?: string; debugEnabled?: boolean } = {}) => {
+  const logger = ref(createLogger({ name, debugEnabled: debugEnabled ?? true }));
+
+  return logger.value;
+};
