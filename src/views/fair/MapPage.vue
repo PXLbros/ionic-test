69303716f3c0fa2cfffa8959f764a823393880d7
majesticblvd <dennis@dreamhuman.com>
2025-04-15 14:14:06 -0700

    Use 1x1 webp image to detect support

--- src/views/fair/MapPage.vue
+++ src/views/fair/MapPage.vue
@@ -1253,10 +1253,23 @@ function loadMapOverlayImage(): Promise<void> {
   });
 }
 
-function checkWebPSupport(): boolean {
-  const canvas = document.createElement('canvas');
+function checkWebPSupport(): Promise<boolean> {
+  return new Promise((resolve) => {
+    const img = new Image();
 
-  return canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+    img.onload = function() {
+      // If the image loads successfully, WebP is supported
+      resolve(true);
+    };
+
+    img.onerror = function() {
+      // If the image fails to load, WebP is not supported
+      resolve(false);
+    };
+
+    // Try to load a 1x1 pixel WebP image
+    img.src = '/icons/1x1.webp';
+  });
 }
 
 // Function to handle cluster clicks
